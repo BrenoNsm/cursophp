76761fd9becb0bf62b99878732d99f1a7b3fb2f6
Breno Nascimento <brenonsmacedo@gmail.com>
2023-06-14 19:13:01 -0300

    Variáveis é constantes

---
+++ ex03/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Var/Cost</title>
+</head>
+<body>
+    <h1>Variáveis é constantes</h1>
+    
+    <?php 
+
+    //Variável ex
+    $nome = "Breno";
+    $sobrenome= "Nascimento";
+
+    
+
+    //Constante ex
+
+    const Pais = "Brasil";
+
+    echo "nome: $nome $sobrenome, do país ".   Pais;
+
+    ?>
+
+</body>
+</html>
